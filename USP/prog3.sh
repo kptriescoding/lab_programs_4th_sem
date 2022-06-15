c63174a139f56159993ff2ee21f5fc03f2024100
kptriescoding <karthikpai08@gmail.com>
2022-06-15 22:00:38 +0530

    Update Programs

--- USP/prog3.sh
+++ USP/prog3.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "The files in the system are"
 if test -d $1
 then
-echo "$(ls -lR $1)"
+echo "The files in the system are"
+ls -lR $1
 else 
 echo "Invalid Directory"
+exit
+fi
 echo "Enter the permission of the file to be read"
 read per
-echo "$( ls -lR $1|grep -e $per )"
+ls -lR $1|grep -e $per
