0db1b0bae758d2ddd1b5dca0ac6aeccc86ce59b7
Dileep Sharma <84862839+dileep-shar@users.noreply.github.com>
2022-07-22 00:55:53 +0530

    Create Prog4.java

---
+++ OOPs/src/Program4/Prog4.java
@@ -0,0 +1,12 @@
+import Progam4.ISE.ThirdSem;
+import Program4.RVCE.Ise_Dept;
+
+public class Program4 {
+    public static void main(String[] args) {
+        ThirdSem t = new ThirdSem();
+        t.greet();
+        Ise_Dept ise = new Ise_Dept();
+        ise.greet();
+
+    }
+}
