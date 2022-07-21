eb626bfe8db1a88eed999be7f1d89ed45249f25c
Dileep Sharma <84862839+dileep-shar@users.noreply.github.com>
2022-07-22 00:58:25 +0530

    Create Ise_Dept.java

---
+++ OOPs/src/Program4/RVCE/Ise_Dept.java
@@ -0,0 +1,9 @@
+import OOPS.prog4.ISE.ThirdSem;
+
+public class Ise_Dept extends ThirdSem {
+
+    @Override
+    public void greet() {
+        System.out.println("Hello students welcome to ISE  Dept");
+    }
+}
