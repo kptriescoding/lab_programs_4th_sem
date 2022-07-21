e9b1f74105caa1544e95980e84b36e4a34efea67
Dileep Sharma <84862839+dileep-shar@users.noreply.github.com>
2022-07-22 00:57:01 +0530

    Create ThirdSem.java

---
+++ OOPs/src/Program4/ISE/ThirdSem.java
@@ -0,0 +1,10 @@
+package Program4.ISE;
+
+public class ThirdSem {
+    public ThirdSem(){
+        System.out.println("Welcome to ISE dept-3rd sem young budding engineers");
+    }
+    public void greet(){
+        System.out.println("hello everyone we welcome you to fourth sem");
+    }
+}
